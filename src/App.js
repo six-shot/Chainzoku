3fc2d8513e328eb41843cf8ee0fd62ea48f81857
six-shot <okhuomonajayi54@gmail.com>
2025-01-04 23:24:19 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Hero from './modules/home/sections/hero';
+
+function App() {
+  return (
+    <div>
+      <Hero/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/modules/home/sections/hero/index.jsx
@@ -0,0 +1,19 @@
+import { motion, useScroll, useTransform } from "framer-motion";
+import React, { useRef } from "react";
+import bg from "../../../../assets/background.jpg";
+import bg2 from "../../../../assets/blob_https___chainzoku.io_3de7a21b-5599-4eb7-80e2-6d1c8fcc6c22";
+export default function MultiLayerParallax() {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+  const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const textY = useTransform(scrollYProgress, [0, 1], ["0%", "200%"]);
+
+  return (
+    <div>
+      <img src={bg2} className="sticky top-0" />
+    </div>
+  );
+}
