8d17f345ee54bcbfed454f212df29c03cabd2ba4
six-shot <okhuomonajayi54@gmail.com>
2025-01-07 02:13:12 +0100

    first commit

--- src/components/global/navbar/index.jsx
+++ src/components/global/navbar/index.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
-import chainzoku from "../../../assets/icons/chainzoku.svg"
+import React from "react";
+import chainzoku from "../../../assets/icons/chainzoku.svg";
+import discord from "../../../assets/icons/discord.svg";
+import instagram from "../../../assets/icons/instagram.svg";
+import twitter from "../../../assets/icons/twitter.svg";
+import zokus from "../../../assets/icons/zokus.svg";
 
 export default function Navbar() {
   return (
-    <div className="fixed top-5 z-[1000] w-full px-5">
+    <div className="fixed top-5 z-[1000] w-full ">
       <div className="flex justify-between items-center px-[2.8125rem] h-[80px]">
-        <img className='w-[123px]' src={chainzoku} />
+        <img className="w-[123px]" src={chainzoku} />
+        <nav>
+          <ul className="flex items-center font-bold italic">
+            <li className="bg-[#cdfb52] h-[29px] rounded-[15px] px-[.625rem] leading-[1.2] flex items-center ">
+              Home
+            </li>
+            <li className="px-[.625rem] h-[29px] text-[#fffff7] leading-[1.2] flex items-center ">
+              Auctions
+            </li>
+            <li className="px-[.625rem] h-[29px] text-[#fffff7] leading-[1.2] flex items-center ">
+              Box Reveal
+            </li>
+            <li className="px-[.625rem] h-[29px] text-[#fffff7] leading-[1.2] flex items-center ">
+              Lore
+            </li>
+            <li className="px-[.625rem] h-[29px] text-[#fffff7] leading-[1.2] flex items-center ">
+              My Zoku
+            </li>
+            <li className="px-[.625rem] h-[29px] text-[#fffff7] leading-[1.2] flex items-center ">
+              Jumps
+            </li>
+            <li className="px-[.625rem] h-[29px] text-[#fffff7] leading-[1.2] flex items-center ">
+              Shops
+            </li>
+          </ul>
+        </nav>
+        <div>
+          <ul className="flex items-center gap-[.9375rem]">
+            <li>
+              <img src={discord} alt="discord" />
+            </li>
+            <li>
+              <img src={twitter} alt="discord" />
+            </li>
+            <li>
+              <img src={instagram} alt="discord" />
+            </li>
+            <li>
+              <img src={zokus} alt="discord" />
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/modules/home/sections/hero/index.jsx
+++ src/modules/home/sections/hero/index.jsx
@@ -1,53 +1,4 @@
-// import { motion, useScroll, useTransform } from "framer-motion";
-// import React, { useRef } from "react";
-// import bg from "../../../../assets/background.jpg";
-// import bg2 from "../../../../assets/blob_https___chainzoku.io_3de7a21b-5599-4eb7-80e2-6d1c8fcc6c22";
-// import Navbar from "../../../../components/global/navbar";
-// import chainzoku from "../../../../assets/icons/chainzoku.svg";
 
-// export default function MultiLayerParallax() {
-//   const ref = useRef(null);
-
-//   // Use Framer Motion's scroll hooks
-//   const { scrollYProgress } = useScroll({
-//     target: ref,
-//     offset: ["start start", "end start"],
-//   });
-
-//   // Parallax effect for the backgrounds
-//   const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "50%"]); // Adjust the range as needed
-//   const foregroundY = useTransform(scrollYProgress, [0, 1], ["0%", "-20%"]);
-
-//   return (
-//     <div ref={ref} className="relative h-[230vh] overflow-hidden">
-//       {/* Background Layer */}
-//       <motion.div
-//         className="absolute inset-0 bg-center"
-//         style={{
-//           backgroundImage: `url(${bg})`,
-//           y: backgroundY,
-//         }}
-//       ></motion.div>
-
-//       {/* Navbar */}
-//       <div className="relative z-10">
-//         <Navbar />
-//       </div>
-
-//       {/* Foreground Layer */}
-//       <motion.img
-//         src={bg2}
-//         className="absolute top-0 left-0 w-full pt-[150px] z-[30]"
-//         style={{ y: foregroundY }}
-//       />
-
-//       {/* Content */}
-//       <div className="relative z-20 flex items-center h-screen justify-center text-white">
-//         <img className="w-[1280px]" src={chainzoku} />
-//       </div>
-//     </div>
-//   );
-// }
 
 
 
@@ -101,11 +52,41 @@ export default function MultiLayerParallax() {
 
       {/* Content */}
       <motion.div
-        className="relative z-20 flex items-center h-screen justify-center text-white"
+        className="relative z-20 flex  flex-col items-center h-screen justify-center text-white"
         style={{ y: contentY }} // Add parallax effect to content
       >
-        <img className="w-[1280px]" src={chainzoku} />
+        <div className=" relative flex items-center justify-center h-[480px]">
+          <img className="w-[1280px]" src={chainzoku} />
+          <h1 className="absolute -bottom-10 agbuch_bold text-[.875rem] leading-[.875rem] uppercase tracking-[-.02em] text-center ">
+            Mint your Zoku. Take a side.
+          </h1>
+        </div>
       </motion.div>
+      <div className="fixed left-[2.8125rem] top-1/2 -translate-y-1/2">
+        <ul className="ulll relative flex flex-col gap-[2.5rem] pl-[1.625rem] agbuch_bold  text-[#fffff7] text-[.6rem] leading-[0.875rem]">
+          <li>
+            <button className="relative btnnn  uppercase">Zokus</button>
+          </li>
+          <li>
+            <button className="relative btnnnn uppercase">Clans</button>
+          </li>
+          <li>
+            <button className="relative btnnnn uppercase">Vision</button>
+          </li>
+          <li>
+            <button className="relative btnnnn uppercase">Custom</button>
+          </li>
+          <li>
+            <button className="relative btnnnn uppercase">Roadmap</button>
+          </li>
+          <li>
+            <button className="relative btnnnn uppercase">Team</button>
+          </li>
+          <li>
+            <button className="relative btnnn uppercase">FAQ</button>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
