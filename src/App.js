751bdd527ec690fe366e50dfd3c82624e2c4d42f
six-shot <okhuomonajayi54@gmail.com>
2025-01-09 01:48:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Hero from "./modules/home/sections/hero";
-import Holder from "./modules/home/sections/holder";
+import Holder from "./modules/home/sections/hero/holder";
+
 
 function App() {
   return (
     <div>
       <Hero />
-     
+     <Holder/>
     </div>
   );
 }
--- src/modules/home/sections/hero/holder/index.jsx
+++ src/modules/home/sections/hero/holder/index.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import holder from "../../../../../assets/43b9039f-bf0ab2ac.mp4";
 
 export default function Holder() {
   return (
     <div className="">
       {" "}
-      {/* <video
+      <video
         src={holder}
         autoPlay
         loop
         muted
         playsInline
         
-      ></video> */}
+      ></video>
     </div>
   );
 }
